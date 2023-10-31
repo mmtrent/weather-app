cc617b8c3577e4267c6752ce9a350c4719f0f84d
Mark Trent <mmtrent@gmail.com>
2023-10-30 22:10:25 -0700

    install and initialize npm

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("Hi"),(async()=>{const o=await async function(){const o=await fetch("https://api.weatherapi.com/v1/current.json?key=c7c264a3ef2c42fab4801504233010&q=south_lake_tahoe",{mode:"cors"}),c=await o.json();return console.log(c),c}();console.log(o)})()})();
